3d43fe86fa8ea5ffdeb8231079bb070547160018
matsouza <matsouza@uolinc.com>
2020-09-09 23:04:34 -0300

    feat: Conflicts solve

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,16 +1,8 @@
 import React from 'react'
-<<<<<<< HEAD
 import { Home, Login, Admin, Products, Product } from '../pages/index'
 import ProtectedRoute from './ProtectedRouter'
-=======
-import Home from '../pages/Home'
-import Login from '../pages/Login'
 import UserRegistry from '../pages/UserRegistry'
 import Cart from '../pages/Cart'
-
-import Admin from '../pages/Admin'
-
->>>>>>> 3831fc638f74c6e9c4ec94a087efe5031ea02c24
 import {
   BrowserRouter,
   Switch,
@@ -32,14 +24,11 @@ export default function Router() {
           <Route exact path='/register'>
             <UserRegistry />
           </Route>
-<<<<<<< HEAD
           <Route exact path='/login' component={Login} />
           <ProtectedRoute exact path="/admin" component={Admin} />
-=======
           <Route exact path='/admin'>
             <Admin />
           </Route>
->>>>>>> 3831fc638f74c6e9c4ec94a087efe5031ea02c24
         </Switch>
       </BrowserRouter>
     </>
